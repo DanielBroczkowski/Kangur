61eed24cef93841ee50457aadbb280170a2d283a
karneol10 <karneoll12@gmail.com>
2020-04-20 20:50:53 +0200

    Add QR scanner + some fixes

--- app/src/main/java/com/example/kangur/view/message_activity/MessageActivity.kt
+++ app/src/main/java/com/example/kangur/view/message_activity/MessageActivity.kt
@@ -1,8 +1,11 @@
 package com.example.kangur.view.message_activity
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -12,6 +15,7 @@ import com.example.kangur.firebase.FirebaseAuthManager
 import com.example.kangur.model.User
 import com.example.kangur.view.latest_message_activity.LatestMessagesActivity
 import com.example.kangur.viewmodel.UsersCommunicationViewModel
+import com.google.zxing.integration.android.IntentIntegrator
 import kotlinx.android.synthetic.main.activity_message.*
 
 class MessageActivity : AppCompatActivity() {
@@ -46,5 +50,27 @@ class MessageActivity : AppCompatActivity() {
             }
         }
 
+        //QR code scanner
+        takePhotoImageButton.setOnClickListener{
+            val scanner = IntentIntegrator(this)
+
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK){
+            val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+            if(result != null) {
+                if(result.contents == null) {
+                    Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                } else {
+                    //Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+                    test_to_send?.setText(result.contents.toString())
+                }
+            } else {
+                super.onActivityResult(requestCode, resultCode, data);
+            }
+        }
     }
 }
